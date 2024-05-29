da0520fd5ab2f05332d9e28fcb31069764518975
evolutis101 <36013919+evolutis101@users.noreply.github.com>
2024-05-28 22:19:55 -0700

    Add files via upload

---
+++ SQL1.sql
@@ -0,0 +1,14 @@
+/* 
+created by Elena Montalvo
+made 05/28/2024 
+*/ 
+-- this is not case sensative NOTE: 
+
+select -- now that i am in the Customer folder extract FirstName, LastName, Email 
+FirstName,
+LastName, 
+Email 
+
+from -- point into the direction where the date is 
+
+Customer
